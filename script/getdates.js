7601e749863dcda11245d600faade60f331e01a4
Kap0812 <dortegakap0812@gmail.com>
2024-09-23 23:37:17 -0600

    first commit

---
+++ script/getdates.js
@@ -0,0 +1,5 @@
+const currentYear = new Date().getFullYear();
+document.getElementById('currentyear').textContent = currentYear;
+
+// Display the date the document was last modified
+document.getElementById('lastModified').textContent = `Last Modified: ${document.lastModified}`;
